f4c53a429c533949e2ba5621ffa55969d9c11863
AURSSS <99349390+AURSSS@users.noreply.github.com>
2022-02-09 17:29:17 +0100

    Upload website

---
+++ connection.php
@@ -0,0 +1,18 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+
+//$servername = "localhost";
+//$username = "c98l9GRLrq";
+//$password = "usyd2f5kCr";
+
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=workfile", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch(PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
